9f20b0fa32506dd7e15d333be0b1efa9879bcc5a
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-02-18 12:44:14 +0100

    Added fake IHeater function

--- ECS.Legacy/FakeHeater.cs
+++ ECS.Legacy/FakeHeater.cs
@@ -8,14 +8,16 @@ namespace ECS.Legacy
 {
     class FakeHeater : IHeater
     {
+        public int TurnOnCount { get; private set;}
+        public int TurnOffCount { get; private set; }
         public void TurnOn()
         {
-
+            TurnOnCount++;
         }
 
         public void TurnOff()
         {
-
+            TurnOffCount++;
         }
 
         public bool RunSelfTest()
