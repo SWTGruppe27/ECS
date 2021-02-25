93bd1d8a97619c9588cd0c83b647d5ca124d1a88
Kathrine <61585423+Kat1606@users.noreply.github.com>
2021-02-25 13:17:47 +0100

    New unittest added. NSubstitute used.

--- ECS.Test.Unit.NSubstitute/UnitTest1.cs
+++ ECS.Test.Unit.NSubstitute/UnitTest1.cs
@@ -1,18 +1,94 @@
+using NSubstitute;
 using NUnit.Framework;
+using ECS.Legacy;
 
 namespace ECS.Test.Unit.NSubstitute
 {
     public class Tests
     {
+        private Ecs uut;
+        private IHeater uutHeater;
+        private ITempSensor uutTempSensor;
+        private IWindow uutWindow;
+
         [SetUp]
         public void Setup()
         {
+            uutHeater = Substitute.For<IHeater>();
+            uutTempSensor = Substitute.For<ITempSensor>();
+            uutWindow = Substitute.For<IWindow>();
+
+            uut = new Ecs(25, 30, uutTempSensor, uutHeater, uutWindow);
         }
 
         [Test]
-        public void Test1()
+        public void GetThreshold_Threshold_Is10()
         {
-            Assert.Pass();
+            uut.SetThreshold(10);
+            Assert.That(uut.GetThreshold(), Is.EqualTo(10));
         }
+
+        [Test]
+        public void setThreshold_Threshold_Is5()
+        {
+            uut.SetThreshold(5);
+            Assert.That(uut.GetThreshold(), Is.EqualTo(5));
+        }
+
+        [Test]
+        public void getTemp_temp_Is10()
+        {
+            uutTempSensor.GetTemp().Returns(10);
+            Assert.That(uut.GetCurTemp(), Is.EqualTo(10));
+        }
+
+        [Test]
+        public void RunSelfTest_retuns_true()
+        {
+            uutTempSensor.RunSelfTest().Returns(true);
+            uutHeater.RunSelfTest().Returns(true);
+            Assert.That(uut.RunSelfTest, Is.True);
+        }
+
+        [Test]
+        public void RunSelfTest_retuns_OneIsFalse()
+        {
+            uutTempSensor.RunSelfTest().Returns(true);
+            uutHeater.RunSelfTest().Returns(false);
+            Assert.That(uut.RunSelfTest, Is.False);
+        }
+
+        [Test]
+        public void Regulate_TempBelowThreshold_HeaterTurnedOn()
+        {
+            uutTempSensor.GetTemp().Returns(20);
+            uut.Regulate();
+            uutHeater.Received(1).TurnOn();
+        }
+
+        [Test]
+        public void Regulate_TempOverThreshold_HeaterTurnedOff()
+        {
+            uutTempSensor.GetTemp().Returns(26);
+            uut.Regulate();
+            uutHeater.Received(1).TurnOff();
+        }
+
+        [Test]
+        public void Regulate_TempOverUpperThreshold_OpenWindow()
+        {
+            uutTempSensor.GetTemp().Returns(31);
+            uut.Regulate();
+            uutWindow.Received(1).Open();
+        }
+
+        [Test]
+        public void Regulate_TempUnderUpperThreshold_CloseWindow()
+        {
+            uutTempSensor.GetTemp().Returns(29);
+            uut.Regulate();
+            uutWindow.Received(1).Close();
+        }
+
     }
 }
